74e0b061c514c3501afc8f9d93ca785aa160c82a
taucher4000 <martin@familie-boehme.eu>
2025-10-10 20:36:40 +0200

    merge latest change from testing branch

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,23 @@
 ARG BUILD_FROM
 FROM $BUILD_FROM
 
-# Install dependencies, create venv, upgrade pip, install inetbox.py
-RUN apk add --no-cache python3 py3-pip git \
-    && python3 -m venv /inetbox \
-    && /inetbox/bin/pip install --upgrade pip \
-    && /inetbox/bin/pip install git+https://github.com/danielfett/inetbox.py@master \
-    && mkdir -p /inetbox/mqtt_auto_discovery_objs
-
 # Copy configuration and discovery objects
-COPY inetbox.yml /etc/miqro.yml
-COPY mqtt_auto_discovery_objs /inetbox/mqtt_auto_discovery_objs/
+COPY src /src
+RUN mkdir /inetbox
 COPY run.py /inetbox/
 RUN chmod a+x /inetbox/run.py
 
+
+# Install dependencies, create venv, upgrade pip, install inetbox.py
+RUN apk add --no-cache python3 py3-pip git \
+    && python3 -m venv /inetbox \
+    && /inetbox/bin/pip install --upgrade pip 
+    #&& /inetbox/bin/pip install git+https://github.com/taucher4000/inetbox.py@master
+    
 # Add venv to PATH
 ENV PATH="/inetbox/bin:$PATH"
+
+RUN /inetbox/bin/pip install -e /src
 
 # Start the application
 CMD ["/inetbox/run.py"]
--- run.py
+++ run.py
@@ -12,8 +12,25 @@ import random
 
 CONFIG_FILE = Path("/etc/miqro.yml")
 OPTIONS_FILE = Path("/data/options.json")
-DISCOVERY_DIR = Path("/inetbox/mqtt_auto_discovery_objs/")
-
+DISCOVERY_DIR = Path("/src/mqtt_auto_discovery_objs/")
+MIQRO_CONFIG = {
+    "broker": {
+        "host": "core-mosquitto",
+        "port": 1883,
+        "keepalive": 60
+    },
+    "auth": {
+        "username": "",
+        "password": ""
+    },
+    "log_level": "INFO",
+    "services": {
+        "truma": {
+            "serial_device": "/dev/serial0",
+            "default_target_temp_room": 22
+        }
+    }
+}
 
 def load_yaml(path: Path) -> dict:
     with path.open("r", encoding="utf-8") as f:
@@ -29,18 +46,18 @@ def load_json(path: Path) -> dict:
     with path.open("r", encoding="utf-8") as f:
         return json.load(f)
 
-
-
-miqro_config = load_yaml(CONFIG_FILE)
 ha_options = load_json(OPTIONS_FILE)
 
-miqro_config["broker"]["host"] = ha_options["MQTTBroker"]
-miqro_config["auth"]["username"] = ha_options["MQTTUser"]
-miqro_config["auth"]["password"] = ha_options["MQTTPassword"]
-miqro_config["services"]["truma"]["default_target_temp_room"] = ha_options["DefaultTargetTempRoom"]
-miqro_config["services"]["truma"]["serial_device"] = ha_options["SerialDevice"]
+MIQRO_CONFIG["broker"]["host"] = ha_options["MQTTBroker"]
+MIQRO_CONFIG["auth"]["username"] = ha_options["MQTTUser"]
+MIQRO_CONFIG["auth"]["password"] = ha_options["MQTTPassword"]
+MIQRO_CONFIG["services"]["truma"]["default_target_temp_room"] = ha_options["DefaultTargetTempRoom"]
+MIQRO_CONFIG["services"]["truma"]["serial_device"] = ha_options["SerialDevice"]
+MIQRO_CONFIG["services"]["truma"]["debug_app"] = ha_options["DebugApp"]
+MIQRO_CONFIG["services"]["truma"]["debug_lin"] = ha_options["DebugLin"]
+MIQRO_CONFIG["services"]["truma"]["debug_protocol"] = ha_options["DebugProtocol"]
 
-save_yaml(miqro_config, CONFIG_FILE)
+save_yaml(MIQRO_CONFIG, CONFIG_FILE)
 
 client = mqtt_client.Client(f'publish-{random.randint(0, 1000)}')
 client.username_pw_set(ha_options["MQTTUser"], ha_options["MQTTPassword"])
